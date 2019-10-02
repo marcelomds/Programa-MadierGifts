1206bff90d325740968e73c4a459be6f3752ce8d
Marcelo Moreira <marcelo_mmds@hotmail.com>
2019-10-01 23:42:31 -0300

    Primeiro Commit: Iniciando o programa MadierGifts

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//Limpando Campos e recarregando a página
+limparCampos = ()=> {
+    document.getElementById('inLargura').value = '';
+    document.getElementById('inComprimento').value = '';
+    document.getElementById('inAltura').value = '';
+    document.getElementById('outValor').value = '';
+    //window.location = ''
+    // Posiciona o cursor no campo 'Largura'
+    document.getElementById('inLargura').focus();
+}
+
+calcularValor = () => {
+    //Receber as medidas da caixa
+    let inLargura = document.getElementById('inLargura');
+    let inComprimento = document.getElementById('inComprimento');
+    let inAltura = document.getElementById('inAltura');
+    let outValor = document.getElementById('outValor');
+
+    //Transformar os dados em números
+    let largura = Number(inLargura.value);
+    let comprimento = Number(inComprimento.value);
+    let altura = Number(inAltura.value);
+
+    // Verificar se o campo compra é vazio (0) ou NaN;
+    if (largura == 0 || isNaN(largura)) {
+        alert('Por favor, digite um valor real para a largura da caixa!');
+        inLargura.value = '';
+        inLargura.focus();
+        return;
+    }
+    if (comprimento == 0 || isNaN(comprimento)) {
+        alert('Por favor, digite um valor real para o comprimento da caixa!');
+        inComprimento.value = '';
+        inComprimento.focus();
+        return;
+    }
+    if (altura == 0 || isNaN(altura)) {
+        alert('Por favor, digite um valor real para a altura da caixa!');
+        inAltura.value = '';
+        inAltura.focus();
+        return;
+    }
+
+    //Mostra o valor total a ser Pago
+    outValor.innerHTML = ()
+}
+
+// Cria referência ao elemento btCalcular e registra evento assossiado a calcular Valor R$
+let resultado = document.getElementById('btCalcular').addEventListener('click', (calcularValor))
+
+
+
+
+
+
+
+
+
+// Cria referência ao elemento btLimpar e registra evento assossiado a limpar campos
+let limpar = document.getElementById('btLimpar').addEventListener('click', (limparCampos));
